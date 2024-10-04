338da9052c48de1e8996da3f05ad24c2f830fc2d
Yuri Ppo <yuri.prata12@gmail.com>
2024-10-04 16:00:25 -0700

    first commit

---
+++ galeria/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+
+from galeria.models import Fotografia
+
+def index(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
